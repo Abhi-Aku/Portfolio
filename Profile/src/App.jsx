2dedb8ce77994123180887c2d433d65ffa3e8c4b
Abhi-Aku <abhi2003av@gmail.com>
2025-03-07 13:52:32 +0530

    gvghgk
    jkj

--- Profile/src/App.jsx
+++ Profile/src/App.jsx
@@ -1,17 +1,11 @@
 import React from 'react'
-import Home from './Components/Home'
-import {BrowserRouter,  Routes, Route } from 'react-router-dom'
 
 const App = () => {
   return (
-    <>
-     <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Home />} />
-        </Routes>
-        </BrowserRouter>
-    </>
-  )
-}
+<>
+
+
+</>  
+)}
 
 export default App
