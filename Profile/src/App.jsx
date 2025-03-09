6b04c3d13bcdfca3e80c17423a29bba7c5473032
Abhi-Aku <abhi2003av@gmail.com>
2025-03-09 22:23:50 +0530

    data
    Update

--- Profile/src/App.jsx
+++ Profile/src/App.jsx
@@ -1,11 +1,24 @@
-import React from 'react'
+import React from 'react';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import Layout from './Components/Layout';
+import Home from './Pages/Home';
+import About from './Pages/About';
+import Contact from './Pages/Contact'; 
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Layout />,
+    children: [
+      { path: "/home", element: <Home /> },
+      { path: "/about", element: <About /> },
+      { path: "/contact", element: <Contact /> }, 
+    ],
+  },
+]);
 
 const App = () => {
-  return (
-<>
+  return <RouterProvider router={router} />;
+};
 
-
-</>  
-)}
-
-export default App
+export default App;
